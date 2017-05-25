556ced5a81e9291495f0c85c7075f319e61d6281
oroka <kmtary13@gmail.com>
2017-05-25 22:53:56 +0900

    5/25
    LED 点灯パターン

---
+++ ledout1.c
@@ -0,0 +1,20 @@
+/*
+ * ŠT—v
+ * 
+ */
+
+#include "iodefine.h"
+
+void InitIO(void);
+void InitLedOut(void);
+void RunLedOut(void);
+
+unsigned char led_data = 0xFF;
+
+void InitIO(){
+	
+}
+
+void RunLedOut(){
+	
+}
